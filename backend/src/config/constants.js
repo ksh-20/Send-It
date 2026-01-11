0474dd7dccfecd9eb3893bde1272adbe23187a3e
Kshitij <kshitij24.srinivasan@gmail.com>
2026-01-11 16:46:30 +0530

    Modified PIN Time Limit to 2 minutes

--- backend/src/config/constants.js
+++ backend/src/config/constants.js
@@ -1,3 +1,3 @@
 export const MAX_FILE_SIZE = 5 * 1024 * 1024; // 5MB
-export const PIN_EXPIRY_MS = 5 * 60 * 1000; // 5 minutes
+export const PIN_EXPIRY_MS = 2 * 60 * 1000; // 2 minutes
 export const UPLOAD_DIR = "src/temp/uploads";
